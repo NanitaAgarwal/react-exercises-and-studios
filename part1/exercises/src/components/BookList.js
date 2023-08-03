382d2e1e23c6d72888bea50493d890cb9b7e6a46
Omkar <omkarred@gmail.com>
2023-08-03 00:13:26 -0500

    React Part-1 Exercises

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "The Book List";
+   let book1 = "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTsb8mYbbnQLjJYN5gL4H79toS0ZWGqCM6kokB2Qah5jDDw7YNNXbo2CVRiQxGUimOp365_8wugonuzMoZnDcPF0wxclzSc64EkhaFEVQ&usqp=CAc";
+   let book2 = "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSncDfyIun1WY-NJb8Y_SFjnSnC-qx1o7GugNQF9yNdZNLx8-nxs1Ds0yxOsOyho4rp4H-T8YqP5RgmgNT2wqbCT-uHilcziX0rpyYN3CA&usqp=CAc";
+   let book3 = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT7dDNfEy1LGd_uoo03Glm3xThkSbQBMChBOkW7L9lLGRZZgSGybBv0Ig7LFS4x229mL4UDDiLN3Wertbh6pPAhUrpoTlHCSBxD9KClcZHIda6EvwKUIo8T&usqp=CAc";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Undefeated Mind" />
+         <img src={book2} alt="TRUST" />
+         <img src={book3} alt="THE SURGEON" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,10 @@
+import styles from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+      <div>
+         <h1 className={styles.choresHeading}>Chores List</h1>
+         <p className={styles.choresText}>No content is here!</p>
+      </div>
+   );
 }
